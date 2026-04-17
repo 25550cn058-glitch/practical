ad7fb22f2dfed6a4d05a5072c74397fb9b064f61
25550cn058-glitch <25550cn058@glbctm.ac.in>
2026-04-17 23:50:36 +0530

    Delete PyramidPattern.java

--- PyramidPattern.java
+++
@@ -1,1 +0,0 @@
-class PyramidPattern{public static void main(String[]a){int n=5;for(int i=1;i<=n;i++){for(int j=i;j<n;j++)System.out.print(" ");for(int j=1;j<=i;j++)System.out.print(j);for(int j=i-1;j>=1;j--)System.out.print(j);System.out.println();}}}
