10d06fd8a363fa5e2d50b0f98a6751e20eb93af2
25550cn058-glitch <25550cn058@glbctm.ac.in>
2026-04-18 09:30:30 +0530

    Add files via upload

---
+++ FibonacciCLI.java
@@ -0,0 +1,12 @@
+public class FibonacciCLI {
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        int a = 0, b = 1;
+        for (int i = 1; i <= n; i++) {
+            System.out.print(a + " ");
+            int c = a + b;
+            a = b;
+            b = c;
+        }
+    }
+}
